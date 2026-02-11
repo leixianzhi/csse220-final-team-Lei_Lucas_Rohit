592a86a8b6f8c6211dbe8a4556812394f8afe649
chiramrj <chiramrj@chiramrj-linux-ThinkPad-P1-Gen-6>
2026-02-10 23:02:06 -0500

    Implemented HUD.

--- src/ui/GameComponent.java
+++ src/ui/GameComponent.java
@@ -1,6 +1,7 @@
 package ui;
 
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import javax.swing.JComponent;
@@ -27,7 +28,20 @@ public class GameComponent extends JComponent {
         });
         timer.start();
     }
-
+    
+    private void printHUD(Graphics2D g2d) {
+		
+		Font ogFont = g2d.getFont();
+		
+		g2d.setFont(new Font("Comic Sans MS", Font.PLAIN, 18));
+		
+		g2d.drawString("Lives: " + model.getPlayer().getLives(), 20, 30);
+		g2d.drawString("Score: " + model.getPlayer().getScore(), 20, 60);
+		
+		g2d.setFont(ogFont);	// preserves font for future drawings
+		
+	}
+    
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
@@ -36,6 +50,9 @@ public class GameComponent extends JComponent {
         for (GameObject obj : model.getObjects()) {
             obj.drawOn(g2);
         }
+        
+        printHUD(g2);
+        
     }
 }
 
